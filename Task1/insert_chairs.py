9b32e1f665e5da8899d9c9593aef0499f75a1313
Christopher Høe <christopher.hoee@gmail.com>
2024-03-12 12:16:48 +0100

    fix: fixed insert_data_sql - it now correctly adds everything

--- Task1/insert_chairs.py
+++ Task1/insert_chairs.py
@@ -2,14 +2,16 @@ import sqlite3
 
 
 def connect():
-    return sqlite3.connect('teater.db')
+    return sqlite3.connect("teater.db")
 
 
 def legg_til_stol(sal_id, rad_nr, stol_nr, omraade):
     conn = connect()
     c = conn.cursor()
-    c.execute("INSERT INTO Stol (SalID, Nr, RadNr, Omraade) VALUES (?, ?, ?, ?)",
-              (sal_id, stol_nr, rad_nr, omraade))
+    c.execute(
+        "INSERT INTO Stol (SalID, Nr, RadNr, Omraade) VALUES (?, ?, ?, ?)",
+        (sal_id, stol_nr, rad_nr, omraade),
+    )
     conn.commit()
     conn.close()
 
@@ -18,12 +20,16 @@ def legg_til_transaksjon_og_billett(sal_id, rad_nr, stol_nr, omraade, dato):
     conn = connect()
     c = conn.cursor()
 
-    c.execute("INSERT INTO Transaksjon (Dato, Tidspunkt, AntallBilletter, KundeID, BillettPrisID) VALUES (?, ?, ?, ?, ?)",
-              (dato, '12:00', 1, 1, 1))
+    c.execute(
+        "INSERT INTO Transaksjon (Dato, Tidspunkt, AntallBilletter, KundeID, BillettPrisID) VALUES (?, ?, ?, ?, ?)",
+        (dato, "12:00", 1, 1, 1),
+    )
     transaksjon_id = c.lastrowid
 
-    c.execute("INSERT INTO Billett (StolNr, RadNr, Omraade, SalID, ForestillingDato, TeaterStykkeID, TransaksjonID) VALUES (?, ?, ?, ?, ?, ?, ?)",
-              (stol_nr, rad_nr, omraade, sal_id, dato, 1, transaksjon_id))
+    c.execute(
+        "INSERT INTO Billett (StolNr, RadNr, Omraade, SalID, ForestillingDato, TeaterStykkeID, TransaksjonID) VALUES (?, ?, ?, ?, ?, ?, ?)",
+        (stol_nr, rad_nr, omraade, sal_id, dato, 1, transaksjon_id),
+    )
     conn.commit()
     conn.close()
 
--- Task1/insert_data.sql
+++ Task1/insert_data.sql
@@ -1,17 +1,16 @@
-/* Teater */
-INSERT INTO Teater (Navn) VALUES ("Trondelag teater"); 
-
-/* Sal */
-INSERT INTO Sal (Navn, AntallPlasser, TeaterID) VALUES ("Hovedscenen", 516, 1);
-
-INSERT INTO Sal (Navn, AntallPlasser, TeaterID) VALUES ("Gamle Scene", 332, 1);
 
 
-/* Teaterstykke */
+--Sal 
+INSERT INTO Sal (Navn) VALUES ("Hovedscenen");
+
+INSERT INTO Sal (Navn) VALUES ("Gamle Scene");
+
+
+--Teaterstykke 
 INSERT INTO TeaterStykke (Navn, Sesong, SalID)  VALUES ("Storst av alt er kjaerligheten", "Vaar", 2);
 INSERT INTO TeaterStykke (Navn, Sesong, SalID)  VALUES ("Kongsemnene", "Vaar", 1);
 
-/* Forestilling */
+--Forestilling
 
 --Kongsemnene
 INSERT INTO Forestilling (TeaterStykkeID, Dato, StartTidspunkt) VALUES (2, "2024-03-08", "18:30:00");
@@ -27,8 +26,8 @@ INSERT INTO Forestilling (TeaterStykkeID, Dato, StartTidspunkt) VALUES (1, "2024
 INSERT INTO Forestilling (TeaterStykkeID, Dato, StartTidspunkt) VALUES (1, "2024-03-12", "19:30:00");
 INSERT INTO Forestilling (TeaterStykkeID, Dato, StartTidspunkt) VALUES (1, "2024-03-18", "19:30:00");
 
-/* Akt */
-INSERT INTO Akt (Navn) VALUES (NULL)
+--Akt
+
 
 
 INSERT INTO Akt (TeaterStykkeID, Nummer) VALUES (2, 1);
@@ -37,57 +36,57 @@ INSERT INTO Akt (TeaterStykkeID, Nummer) VALUES (2, 3);
 INSERT INTO Akt (TeaterStykkeID, Nummer) VALUES (2, 4);
 INSERT INTO Akt (TeaterStykkeID, Nummer) VALUES (2, 5);
 
-/* Rolle */
-INSERT INTO Rolle (Navn) Values ("Håkon Håkonsson")
-INSERT INTO Rolle (Navn) Values ("Dagfinn Bond")
-INSERT INTO Rolle (Navn) Values ("Håkon Håkonsson")
-INSERT INTO Rolle (Navn) Values ("Håkon Håkonsson")
-INSERT INTO Rolle (Navn) Values ("Håkon Håkonsson")
-INSERT INTO Rolle (Navn) Values ("Håkon Håkonsson")
-INSERT INTO Rolle (Navn) Values ("Håkon Håkonsson")
-INSERT INTO Rolle (Navn) Values ("Håkon Håkonsson")
-INSERT INTO Rolle (Navn) Values ("Håkon Håkonsson")
-INSERT INTO Rolle (Navn) Values ("Håkon Håkonsson")
-INSERT INTO Rolle (Navn) Values ("Håkon Håkonsson")
-INSERT INTO Rolle (Navn) Values ("Håkon Håkonsson")
+--Rolle
+INSERT INTO Rolle (Navn) Values ("Håkon Håkonsson");
+INSERT INTO Rolle (Navn) Values ("Dagfinn Bond");
+INSERT INTO Rolle (Navn) Values ("Håkon Håkonsson");
+INSERT INTO Rolle (Navn) Values ("Håkon Håkonsson");
+INSERT INTO Rolle (Navn) Values ("Håkon Håkonsson");
+INSERT INTO Rolle (Navn) Values ("Håkon Håkonsson");
+INSERT INTO Rolle (Navn) Values ("Håkon Håkonsson");
+INSERT INTO Rolle (Navn) Values ("Håkon Håkonsson");
+INSERT INTO Rolle (Navn) Values ("Håkon Håkonsson");
+INSERT INTO Rolle (Navn) Values ("Håkon Håkonsson");
+INSERT INTO Rolle (Navn) Values ("Håkon Håkonsson");
+INSERT INTO Rolle (Navn) Values ("Håkon Håkonsson");
 
-/* Skuespillere */
+--Skuespillere
 
-/*  KONGSEMNENE */
-INSERT INTO Skuespiller (AnsattID, Navn, Epost, AnsattStatus) Values ("Arturo Scotti", NULL, "Fast")
-INSERT INTO Skuespiller (AnsattID, Navn, Epost, AnsattStatus) Values ("Ingunn Beate Strige Øyen", NULL, "Fast")
-INSERT INTO Skuespiller (AnsattID, Navn, Epost, AnsattStatus) Values ("Hans Petter Nilsen", NULL, "Fast")
-INSERT INTO Skuespiller (AnsattID, Navn, Epost, AnsattStatus) Values ("Madeleine Brandtzæg Nilsen", NULL, "Fast")
-INSERT INTO Skuespiller (AnsattID, Navn, Epost, AnsattStatus) Values ("Synnøve Fossum Eriksen", NULL, "Fast")
-INSERT INTO Skuespiller (AnsattID, Navn, Epost, AnsattStatus) Values ("Emma Caroline Deichmann", NULL, "Fast")
-INSERT INTO Skuespiller (AnsattID, Navn, Epost, AnsattStatus) Values ("Thomas Jensen Takyi", NULL, "Fast")
-INSERT INTO Skuespiller (AnsattID, Navn, Epost, AnsattStatus) Values ("Per Bogstad Gulliksen", NULL, "Fast")
-INSERT INTO Skuespiller (AnsattID, Navn, Epost, AnsattStatus) Values ("Isak Holmen Sørensen", NULL, "Fast")
-INSERT INTO Skuespiller (AnsattID, Navn, Epost, AnsattStatus) Values ("Fabian Heidelberg Lunde", NULL, "Fast")
-INSERT INTO Skuespiller (AnsattID, Navn, Epost, AnsattStatus) Values ("Emil Olafsson", NULL, "Fast")
-INSERT INTO Skuespiller (AnsattID, Navn, Epost, AnsattStatus) Values ("Snorre Ryen Tøndel", NULL, "Fast")
+-- KONGSEMNENE
+INSERT INTO Skuespiller (Navn, Epost, AnsattStatus) Values ("Arturo Scotti", NULL, "Fast");
+INSERT INTO Skuespiller (Navn, Epost, AnsattStatus) Values ("Ingunn Beate Strige Øyen", NULL, "Fast");
+INSERT INTO Skuespiller (Navn, Epost, AnsattStatus) Values ("Hans Petter Nilsen", NULL, "Fast");
+INSERT INTO Skuespiller (Navn, Epost, AnsattStatus) Values ("Madeleine Brandtzæg Nilsen", NULL, "Fast");
+INSERT INTO Skuespiller (Navn, Epost, AnsattStatus) Values ("Synnøve Fossum Eriksen", NULL, "Fast");
+INSERT INTO Skuespiller (Navn, Epost, AnsattStatus) Values ("Emma Caroline Deichmann", NULL, "Fast");
+INSERT INTO Skuespiller (Navn, Epost, AnsattStatus) Values ("Thomas Jensen Takyi", NULL, "Fast");
+INSERT INTO Skuespiller (Navn, Epost, AnsattStatus) Values ("Per Bogstad Gulliksen", NULL, "Fast");
+INSERT INTO Skuespiller (Navn, Epost, AnsattStatus) Values ("Isak Holmen Sørensen", NULL, "Fast");
+INSERT INTO Skuespiller (Navn, Epost, AnsattStatus) Values ("Fabian Heidelberg Lunde", NULL, "Fast");
+INSERT INTO Skuespiller (Navn, Epost, AnsattStatus) Values ("Emil Olafsson", NULL, "Fast");
+INSERT INTO Skuespiller (Navn, Epost, AnsattStatus) Values ("Snorre Ryen Tøndel", NULL, "Fast");
 
-/* STØRST AV ALT ER KJØRLIGHETEN */
-INSERT INTO Skuespiller (AnsattID, Navn, Epost, AnsattStatus) Values ("Sunniva Du Mond Nordal", NULL, "Fast")
-INSERT INTO Skuespiller (AnsattID, Navn, Epost, AnsattStatus) Values ("Jo Saberniak", NULL, "Fast")
-INSERT INTO Skuespiller (AnsattID, Navn, Epost, AnsattStatus) Values ("Marte M. Steinholt", NULL, "Fast")
-INSERT INTO Skuespiller (AnsattID, Navn, Epost, AnsattStatus) Values ("Tor Ivar Hagen", NULL, "Fast")
-INSERT INTO Skuespiller (AnsattID, Navn, Epost, AnsattStatus) Values ("Trond-Ove Skrødal", NULL, "Fast")
-INSERT INTO Skuespiller (AnsattID, Navn, Epost, AnsattStatus) Values ("Natalie Grøndahl Tangen", NULL, "Fast")
-INSERT INTO Skuespiller (AnsattID, Navn, Epost, AnsattStatus) Values ("Håsmund Flaten", NULL, "Fast")
+--STØRST AV ALT ER KJØRLIGHETEN
+INSERT INTO Skuespiller (Navn, Epost, AnsattStatus) Values ("Sunniva Du Mond Nordal", NULL, "Fast");
+INSERT INTO Skuespiller (Navn, Epost, AnsattStatus) Values ("Jo Saberniak", NULL, "Fast");
+INSERT INTO Skuespiller (Navn, Epost, AnsattStatus) Values ("Marte M. Steinholt", NULL, "Fast");
+INSERT INTO Skuespiller (Navn, Epost, AnsattStatus) Values ("Tor Ivar Hagen", NULL, "Fast");
+INSERT INTO Skuespiller (Navn, Epost, AnsattStatus) Values ("Trond-Ove Skrødal", NULL, "Fast");
+INSERT INTO Skuespiller (Navn, Epost, AnsattStatus) Values ("Natalie Grøndahl Tangen", NULL, "Fast");
+INSERT INTO Skuespiller (Navn, Epost, AnsattStatus) Values ("Håsmund Flaten", NULL, "Fast");
 
 
-/* Bilettpris */ 
+--Bilettpris
 INSERT INTO Gruppe (GruppeType)
 VALUES
-    ('Ordinær'),
-    ('Honnør'),
-    ('Student'), 
+    ("Ordinær"),
+    ("Honnør"),
+    ("Student"), 
     ("Barn"), 
     ("Gruppe 10"), 
     ("Gruppe honnør 10"); 
 
-/* Rolle */
+--Rolle
 INSERT INTO Rolle (Navn) Values 
     ("Haakon Haakonssønn"),
     ("Inga fra Vartejg"), 
@@ -100,7 +99,7 @@ INSERT INTO Rolle (Navn) Values
     ("Gregorius Jonssønn"),
     ("Paal Flida"), 
     ("Baard Bratte"), 
-    ("Tr")
+    ("Tr");
 
 
 
